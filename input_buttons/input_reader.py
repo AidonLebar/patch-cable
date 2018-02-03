2ac375a65ae1d03f12eed6b27145d315a84cc2d0
Aidon Lebar <aidonlebar@gmail.com>
2018-02-03 16:27:13 -0500

    Monitors serial input for change.

--- input_buttons/input_reader.py
+++ input_buttons/input_reader.py
@@ -1,1 +1,9 @@
+import serial
 
+arduino = serial.Serial('/dev/cu.usbmodem1411', 9600)
+prev = arduino.read()
+while(True):
+    buttonIn = arduino.read()
+    if buttonIn != prev:
+        print("{0:b}".format(int.from_bytes(arduino.read(), byteorder='big')))
+        prev = buttonIn
