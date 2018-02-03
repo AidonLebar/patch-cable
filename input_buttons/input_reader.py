73c7bcfd0004e89663e79657092da6df940e1fec
Aidon Lebar <aidonlebar@gmail.com>
2018-02-03 16:44:36 -0500

    Parses input from serial with dummy functionality.

--- input_buttons/input_reader.py
+++ input_buttons/input_reader.py
@@ -1,9 +1,52 @@
 import serial
 
 arduino = serial.Serial('/dev/cu.usbmodem1411', 9600)
-prev = arduino.read()
+prev = int.from_bytes(arduino.read(), byteorder = 'big')
 while(True):
-    buttonIn = arduino.read()
-    if buttonIn != prev:
-        print("{0:b}".format(int.from_bytes(arduino.read(), byteorder='big')))
-        prev = buttonIn
+    controlIn = int.from_bytes(arduino.read(), byteorder = 'big')
+    if controlIn != prev:
+        if controlIn & 0b00000001 == 1:
+            print("button 7.")
+
+        if controlIn & 0b00000010 > 1:
+            print("button 6.")
+
+        if controlIn & 0b00000100 > 1:
+            print("button 5.")
+
+        if controlIn & 0b00001000 > 1:
+            print("button 4.")
+
+        if controlIn & 0b00010000 > 1:
+            print("button 3.")
+
+        if controlIn & 0b00100000 > 1:
+            print("button 2.")
+
+        if controlIn & 0b01000000 > 1:
+            print("button 1.")
+
+
+
+        if controlIn & 0b00000001 == 0 and prev & 0b00000001 != 0:
+            print("button 7 released.")
+
+        if controlIn & 0b00000010 == 0 and prev & 0b00000010 != 0:
+            print("button 6 released.")
+
+        if controlIn & 0b00000100 == 0 and prev & 0b00000100 != 0:
+            print("button 5 released.")
+
+        if controlIn & 0b00001000 == 0 and prev & 0b00001000 != 0:
+            print("button 4 released.")
+
+        if controlIn & 0b00010000 == 0 and prev & 0b00010000 != 0:
+            print("button 3 released.")
+
+        if controlIn & 0b00100000 == 0 and prev & 0b00100000 != 0:
+            print("button 2 released.")
+
+        if controlIn & 0b01000000 == 0 and prev & 0b01000000 != 0:
+            print("button 1 released.")
+
+        prev = controlIn
