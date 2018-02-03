f8e1f60865da37907226d1fe6c7b4cd79ebbf440
Aidon Lebar <aidonlebar@gmail.com>
2018-02-03 13:45:08 -0500

    initial commit. Buttons set up.

---
+++ input_buttons/input_buttons.ino
@@ -0,0 +1,27 @@
+const int buttonPin0 = 4;
+const int buttonPin1 = 5;
+const int buttonPin2 = 6;
+const int buttonPin3 = 7;
+const int buttonPin4 = 8;
+const int buttonPin5 = 9;
+const int buttonPin6 = 10;
+
+int buttonState0 = 0;
+int buttonState1 = 0;
+int buttonState2 = 0;
+int buttonState3 = 0;
+int buttonState4 = 0;
+int buttonState5 = 0;
+int buttonState6 = 0;
+int buttonState7 = 0;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
