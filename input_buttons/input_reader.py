61e53a0c5f7c19851159205cbcd4bd94a450d08a
Aidon Lebar <aidonlebar@gmail.com>
2018-02-03 16:53:05 -0500

    Input parsing only resiters changes.

--- input_buttons/input_reader.py
+++ input_buttons/input_reader.py
@@ -5,25 +5,25 @@ prev = int.from_bytes(arduino.read(), byteorder = 'big')
 while(True):
     controlIn = int.from_bytes(arduino.read(), byteorder = 'big')
     if controlIn != prev:
-        if controlIn & 0b00000001 == 1:
+        if controlIn & 0b00000001 != 0 and prev & 0b00000001 == 0:
             print("button 7.")
 
-        if controlIn & 0b00000010 > 1:
+        if controlIn & 0b00000010 != 0 and prev & 0b00000010 == 0:
             print("button 6.")
 
-        if controlIn & 0b00000100 > 1:
+        if controlIn & 0b00000100 != 0 and prev & 0b00000100 == 0:
             print("button 5.")
 
-        if controlIn & 0b00001000 > 1:
+        if controlIn & 0b00001000 != 0 and prev & 0b00001000 == 0:
             print("button 4.")
 
-        if controlIn & 0b00010000 > 1:
+        if controlIn & 0b00010000 != 0 and prev & 0b00010000 == 0:
             print("button 3.")
 
-        if controlIn & 0b00100000 > 1:
+        if controlIn & 0b00100000 != 0 and prev & 0b00100000 == 0:
             print("button 2.")
 
-        if controlIn & 0b01000000 > 1:
+        if controlIn & 0b01000000 != 0 and prev & 0b01000000 == 0:
             print("button 1.")
 
 
